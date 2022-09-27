4664e6237815d6341aa296af26594cfdafb9834e
Алексей Писарев <afonasiu2983@gmail.com>
2022-09-27 10:56:42 +1000

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PR2_Kuznetsov
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Itog.Text = "Ст. гр. ИСП Кузнецов А.А.";
+        }
+
+        private void Schet_Click(object sender, RoutedEventArgs e)
+        {
+            Itog.Text += Environment.NewLine + "Практическая работа 2";
+            // Считывание значения X
+            double x = double.Parse(X.Text);
+            // Вывод значения X в окно
+            Itog.Text += Environment.NewLine + "X =" + x.ToString();
+            // Считывает значение Y
+            double y = double.Parse(Y.Text);
+            // Вывод значения Y в окно
+            Itog.Text += Environment.NewLine + "Y = " + y.ToString();
+            // Считывание значения Z
+            double z = double.Parse(Z.Text);
+            // Вывод значения Z в окно
+            Itog.Text += Environment.NewLine + "Z = " + z.ToString();
+
+            // Вычисляем арифметическое выражение
+            double a = Math.Tan(x + y) * Math.Tan(x + y);
+            double b = Math.Exp(y - z);
+            double c = Math.Sqrt(Math.Cos(x * x) + Math.Sin(z * z));
+            double u = a - b * c;
+            // Выводим результат в окно
+            Itog.Text += Environment.NewLine + "Результат U = " + u.ToString();
+        }
+    }
+}
